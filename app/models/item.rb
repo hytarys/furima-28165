29d292d2db1b0af466bb859141a570007e99c37b
hytarys <68804163+hytarys@users.noreply.github.com>
2020-09-17 20:52:16 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,13 @@ class Item < ApplicationRecord
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
+
+  validates :name, presence: true, length: { maximum: 40}
+  validates :explanation, presence: true, length { maximum: 1000}
+  validates :category_id, presence: true
+  validates :status_id, presence: true
+  validates :shipping_fee_id, presence: true
+  validates :shipping_origin_id, presence: true
+  validates :shipping_span_id, presence: true
+  validates :price, presence: true, length { maximum: 7}
 end
