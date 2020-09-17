c5e8ccdb7b4a5aad7a71cdbcbc07ee956d2cb427
hytarys <68804163+hytarys@users.noreply.github.com>
2020-09-17 20:57:57 +0900

    バリデーションの再設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,11 +5,12 @@ class Item < ApplicationRecord
   belongs_to_active_hash :category
 
   validates :name, presence: true, length: { maximum: 40}
-  validates :explanation, presence: true, length { maximum: 1000}
+  validates :explanation, presence: true, length: { maximum: 1000}
   validates :category_id, presence: true
   validates :status_id, presence: true
-  validates :shipping_fee_id, presence: true
+  validates :delivery_fee_id, presence: true
   validates :shipping_origin_id, presence: true
   validates :shipping_span_id, presence: true
-  validates :price, presence: true, length { maximum: 7}
+  validates :price, presence: true, length: { maximum: 7}
+  validates :image, presence: true
 end
