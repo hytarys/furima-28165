9ca24f645d94ffd50ba871177289ce1d97911047
hytarys <68804163+hytarys@users.noreply.github.com>
2020-09-19 20:13:03 +0900

    itemモデルの単体テストコードの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,11 +6,11 @@ class Item < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 40}
   validates :explanation, presence: true, length: { maximum: 1000}
-  validates :category_id, presence: true, inclusion: { in: %w(1), message: 'Select'}
-  validates :status_id, presence: true, inclusion: { in: %w(1), message: 'Select'}
-  validates :delivery_fee_id, presence: true, inclusion: { in: %w(1), message: 'Select'}
-  validates :shipping_origin_id, presence: true, inclusion: { in: %w(1), message: 'Select'}
-  validates :shipping_span_id, presence: true, inclusion: { in: %w(1), message: 'Select'}
+  validates :category_id, presence: true, numericality: { :greater_than => 1, message:'Select'}
+  validates :status_id, presence: true, numericality: { :greater_than => 1, message:'Select'}
+  validates :delivery_fee_id, presence: true, numericality: { :greater_than => 1, message:'Select'}
+  validates :shipping_origin_id, presence: true, numericality: { :greater_than => 1, message:'Select'}
+  validates :shipping_span_id, presence: true, numericality: { :greater_than => 1, message:'Select'}
   validates :price, presence: true, :numericality => { :greater_than => 299, :less_than => 10000000}, format: { with: /\A[0-9]+\Z/, message: 'Half-width number'}
   validates :image, presence: true
 end
